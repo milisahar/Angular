7d8af88b9a91c3cfc3f71b94bc2f7ca4d22f8cce
milisahar <milisahar@yahoo.com>
2023-04-13 11:21:17 +0100

    routing1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { OffreComponent } from './offre/offre.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'products', pathMatch:'full'},
   {path:'products',component:ProductComponent},
-  {path:'offres',component:OffreComponent}
+  {path:'offres',component:OffreComponent},
+  {path:'**',component:NotFoundComponent}//**  none of the path above */
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
 import { OffreComponent } from './offre/offre.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     OffreComponent,
-    NavBarComponent
+    NavBarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
